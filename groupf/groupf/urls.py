1a318db4d66163b6748c4379e453c8674f18e772
famimanotentyo <komonofami@gmail.com>
2026-02-10 09:55:41 +0900

    hh

--- groupf/groupf/urls.py
+++ groupf/groupf/urls.py
@@ -2,7 +2,8 @@
 
 #aaaaaああ
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.contrib.staticfiles.views import serve
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -21,3 +22,8 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
     urlpatterns += staticfiles_urlpatterns()
+
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^static/(?P<path>.*)$', serve, {'insecure': True}),
+    ]
